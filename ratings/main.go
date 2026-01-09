9f2a2ae1318a62f851b3d8f43b2458d3b9e5d22f
villanel <1179916619@qq.com>
2026-01-09 08:15:53 +0000

    add httproute

--- ratings/main.go
+++ ratings/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	// 1. 初始化 Tracer (确保配置了全局 Propagator)(
+	// 1. 初始化 Tracer (确保配置了全局 Propagator)
 	shutdown, err := otel.InitTracer("ratings")
 	if err != nil {
 		log.Fatalf("Failed to initialize tracer: %v", err)
